44a28d8ec91655b39b46adb79c379dbfb4ace76e
WEGFan <goofans@qq.com>
2020-03-21 20:22:14 +0800

    refactor

--- GDProfileFix.py
+++ GDProfileFix.py
@@ -1,76 +1,100 @@
 # -*- coding: utf-8 -*-
-
 import base64
+import datetime
 import os
 import shutil
 import struct
-import time
+import sys
+import traceback
 import zlib
+from textwrap import dedent
+from pathlib import Path
 
-saves = ['CCGameManager.dat', 'CCLocalLevels.dat']
+__version__ = '1.1.0'
 
-if __name__ == '__main__':
-    os.chdir(os.getenv('localappdata') + '\\GeometryDash\\')
+SAVEFILE_NAME = ['CCGameManager.dat', 'CCLocalLevels.dat']
+SAVEFILE_PATH = Path(os.getenv('LocalAppData')) / 'GeometryDash'
 
-    print('Geometry Dash Profile Fix v0.1 by WEGFan')
+
+def main():
+    print(dedent(
+        f"""\
+        Geometry Dash Savefile Fix v{__version__} by WEGFan
+
+        This tool can fix most problems caused unable to open the game by the savefiles.
+        (which is the game will only launch if you move the savefiles to other folders)
+
+        If you encounter any problem (unable to fix / doesn't work after fixed),
+        feel free to create an issue or contact me on Discord (WEGFan#1440).
+        """
+    ))
+
+    print(f'Savefiles location: {SAVEFILE_PATH}')
     print()
-    print('This tool can fix most problems caused unable to open the game by the save files.')
-    print('(double clicking the game won\'t bring up any window, but moving the save files to other folders can open the game)')
-    print('If you encounter any problem (unable to fix / doesn\'t work after fixed), feel free to create an issue.')
-    print('Save files location:' + os.getcwd())
-    print()
+
+    os.chdir(SAVEFILE_PATH)
 
     # backup original file
-    bakFolder = f'backup-{int(time.time())}'
-    os.mkdir(bakFolder)
-    for saveFile in saves:
+    backup_folder_name = f"backup_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
+    os.mkdir(backup_folder_name)
+    for savefile in SAVEFILE_NAME:
         try:
-            shutil.copyfile(saveFile, bakFolder + '\\' + saveFile)
-        except:
-            pass
-    print('Save files backed up to ' + os.getcwd() + '\\' + bakFolder)
+            shutil.copyfile(savefile, Path(backup_folder_name, savefile))
+        except Exception as err:
+            print(f'Failed to backup {savefile}!')
+    print(f'Savefiles backed up to {SAVEFILE_PATH / backup_folder_name}')
     print()
 
-    input('Press Enter to fix...')
+    input('Press ENTER to fix...')
     print()
 
-    for saveFile in saves:
+    for savefile in SAVEFILE_NAME:
         try:
-            with open(saveFile, 'rb') as f:
-                saveData = f.read()
+            with open(savefile, 'rb') as f:
+                savedata = f.read()
+
+            print(f'Fixing {savefile}...')
 
             # decrypt
-            decrypted = []
-            for i in saveData:
-                decrypted.append(i ^ 11)
-
-            decryptedString = bytearray(decrypted).decode().replace('-', '+').replace('_', '/')
-            decodedByteArray = base64.b64decode(decryptedString.encode())
-            finString = zlib.decompress(decodedByteArray[10:], -zlib.MAX_WBITS).decode()
+            decrypted_data = bytes(i ^ 11 for i in savedata)
+            decoded_data = base64.b64decode(decrypted_data, altchars='-_')
+            decompressed_data = zlib.decompress(decoded_data[10:], -zlib.MAX_WBITS)
 
             # fix (i don't know why simply add spaces works...)
-            fixed = finString.replace('><', '> <', 50)
+            decompressed_data = decompressed_data.replace(b'><', b'> <', 50)
 
             # encrypt
-            fixedByteArray = fixed.encode()
+            compressed_data = zlib.compress(decompressed_data)
+            data_crc32 = zlib.crc32(decompressed_data)
+            data_size = len(decompressed_data)
 
-            compressedData = zlib.compress(fixedByteArray)
-            crc32 = struct.pack('I', zlib.crc32(fixedByteArray))
-            dataSize = struct.pack('I', len(fixedByteArray))
+            compressed_data = (b'\x1f\x8b\x08\x00\x00\x00\x00\x00\x00\x0b' +  # gzip header
+                               compressed_data[2:-4] +
+                               struct.pack('I I', data_crc32, data_size))
 
-            compressed = b'\x1f\x8b\x08\x00\x00\x00\x00\x00\x00\x0b' + compressedData[2:-4] + crc32 + dataSize
-            encodedByteArray = base64.b64encode(compressed).decode().replace('+', '-').replace('/', '_').encode()
+            encoded_data = base64.b64encode(compressed_data, altchars=b'-_')
+            encrypted_data = bytes(i ^ 11 for i in encoded_data)
 
-            encrypted = []
-            for i in encodedByteArray:
-                encrypted.append(i ^ 11)
-            result = bytearray(encrypted)
+            with open(savefile, 'wb') as f:
+                f.write(encrypted_data)
 
-            with open(saveFile, 'wb') as f:
-                f.write(result)
-        except FileNotFoundError:
-            print('Can\'t find ' + saveFile + '!')
-        except:
-            print('There was an issue fixing ' + saveFile + '!')
+            print(f'{savefile} fixed!')
+        except FileNotFoundError as err:
+            print(f"Can't find {savefile}!")
+        except Exception as err:
+            print(f'There is an issue fixing {savefile}!')
+            traceback.print_exc()
+
     print()
-    input('Done! Press Enter to exit...')
+    input('Done! Press ENTER to exit...')
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except (EOFError, KeyboardInterrupt) as err:
+        sys.exit()
+    except Exception as err:
+        print('Oops, something went wrong...')
+        print('Error message:')
+        traceback.print_exc()
